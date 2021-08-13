dfc58b28325c7b865c26cb96be4ee7d16612ce66
itrack <jhonatheberson@gmail.com>
2021-08-13 16:46:07 -0300

    finished backend

--- 2_unidade/backend.cpp
+++ 2_unidade/backend.cpp
@@ -16,6 +16,7 @@ int options;
 string filter;
 int pid_vitima;
 int cpu;
+int prioridade;
 cpu_set_t  mask;
 
 
@@ -47,7 +48,16 @@ void function_aloc_cpu(int cpu,int pid){
   CPU_ZERO(&mask);
   CPU_SET(cpu-1, &mask);    // alocar na CPU 0
   int result = sched_setaffinity(pid, sizeof(mask), &mask);  // 0 --> Aqui é no próprio processo. No Geral deve ser o PID
-  cout << "\nprocesso com o PID: " << pid << ",foi alocado na segunite cpu: " << cpu << "\n\n";
+  cout << "\nprocesso com o PID: " << pid << ",foi alocado na seguinte cpu: " << cpu << "\n\n";
+};
+void function_aloc_prioridade(int prio,int pid){
+  //0 maior prioridade
+  //5 baixa prioridade
+  //10 muita baixa prioridade
+  // apos reduzir não consigo aumentar prioridade
+  setpriority(PRIO_PROCESS, pid, prio);
+  cout << "\nprocesso com o PID: " << pid << ",foi alocado na seguinte prioridade: " << prio << "\n\n";
+  cout << "valor da prioridade do processo: após segundo setpriority() " << getpriority(PRIO_PROCESS, pid ) <<endl;
 };
 
 
@@ -61,7 +71,8 @@ int main()
     cout << "Digite 2 para suspender o processo\n";
     cout << "Digite 3 para continuar o processo\n";
     cout << "Digite 4 para filtrar pelo nome\n";
-    cout << "Digite 5 para alocarum programa ao CPU definida\n";
+    cout << "Digite 5 para alocar um programa ao CPU definida\n";
+    cout << "Digite 6 para mudar prioridade do processo\n";
     cout << "==================================\n";
     cin >> options;
     switch (options)
@@ -93,6 +104,13 @@ int main()
       printf( "Digite qual PID do processo que você quer alocar a CPU: ");
       cin >> pid_vitima;
       function_aloc_cpu(cpu, pid_vitima);
+      break;
+    case 6:
+      printf( "Digite qual a prioridade do processo: \n");
+      cin >> prioridade;
+      printf( "Digite qual PID do processo que você quer mudar prioridade: ");
+      cin >> pid_vitima;
+      function_aloc_prioridade(prioridade, pid_vitima);
       break;
     
     default:
